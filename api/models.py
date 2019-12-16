94948393dd61478d2f8b2b210899f788086fe578
Pavlos <pavloskoz@hotmail.com>
2019-12-16 12:20:33 +0000

    Django
    * update to model to handle create once and read_only for date_created

--- api/models.py
+++ api/models.py
@@ -6,8 +6,9 @@ class Todo(models.Model):
     title = models.CharField(max_length=60)
     description = models.CharField(max_length=150)
     priority_level = models.CharField(max_length=10)
-    date_created = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.title
